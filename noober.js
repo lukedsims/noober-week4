76c102d5e3691587583a5106b846d89f956d6310
Luke Sims <lukedansims@gmail.com>
2021-02-02 11:00:23 -0600

    First Commit

--- noober.js
+++ noober.js
@@ -4,9 +4,27 @@ async function pageLoaded() {
 
   // writes the returned JSON to the console
   console.dir(json)
+
+  let rides = json
   
   // 🔥 start here: write code to loop through the rides
   
+for (let i=0, i < rides.length; i++) {
+
+let ride = rides [i]
+let levelOfServce
+
+if (ride.length > 1) {
+  levelOfServce = 'Noober Pool'
+} else if (ride[0].purpleRequested == true) {
+  levelOfServce = 'Noober Purple' 
+} else if (ride [0].numberOfPassengers > 3) {
+  levelOfServce = 'NooberX XL'
+} else {levelOfServce = 'Noober X'}
+
+let ridesHTMLElement = document.querySelector('.rides')
+
+}
 }
 
 window.addEventListener('DOMContentLoaded', pageLoaded)
